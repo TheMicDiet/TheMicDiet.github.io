d50842eb660d9fa8e113379aa72451eecb9d8ffe
Michael <info@mi-dietrich.de>
2020-09-19 18:03:29 +0200

    updated infos

--- script.js
+++ script.js
@@ -16,17 +16,4 @@ let typer2 = new TypeIt('#footer-text', {
 }, false);
 
 
-let typer3 = new TypeIt('#about-text', {
-    speed: 20,
-    afterComplete: function(instance) {
-        instance.destroy()
-        typer4.init()
-    }
-}, false)
-let typer4 = new TypeIt('#project-text', {
-    speed: 20,
-    afterComplete: function(instance) {
-        instance.destroy()
-        typer2.init()
-    }
-}, false)
+
